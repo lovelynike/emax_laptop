93695da38055ed8c8d2f1db9c8039aa1963c7208
emaxkang <lovelynike@naver.com>
2023-04-12 13:20:04 +0900

    first commit

---
+++ java_DataStructure/Array_test.java
@@ -0,0 +1,3 @@
+public class Array_test {
+    int[] numbers = {1, 2, 3, 4, 5};
+}
---
+++ java_DataStructure/Main.java
@@ -0,0 +1,17 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jake!");
+        Array_test();
+
+    }
+
+    private static void Array_test() {
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(numbers));
+    }
+}
+
+
