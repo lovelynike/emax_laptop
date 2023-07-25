dbc52c845f60e439e38593b8232efc0b7eaecc84
Jake <lovelynike@naver.com>
2023-07-25 13:39:21 +0900

    w3s java

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,23 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println(3);
+        System.out.println(33+3);
+
+        // This is a comment
+        /*
+         * The code below will print words 
+         * 
+         */
+
+         // git 
+    }
+}
