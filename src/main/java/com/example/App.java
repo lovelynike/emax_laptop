dd2de8bb50e3e9eaaad5742c4e57c13b1ca69d70
Jake <lovelynike@naver.com>
2023-07-27 17:10:48 +0900

    20230727_w3s_java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,16 +8,88 @@ public class App
 {
     public static void main( String[] args )
     {
+        /* 
         System.out.println( "Hello World!" );
         System.out.println(3);
         System.out.println(33+3);
 
         // This is a comment
-        /*
-         * The code below will print words 
-         * 
-         */
+           
 
-         // git 
+         // git 어케하는지 모르겟음..... ㅠ
+
+         String name = "Jake";
+    
+        System.out.println(name);
+
+        // Declare variables of types
+
+        int myNum = 5;
+        float myFloatNum = 5.99f;
+        char myLetter = 'D';
+        boolean myBool = true;
+        String myText = "Hello";
+
+        // ASCII values
+        char myVar1 = 65, myVar2 = 66, myVar3 = 67;
+        System.out.println(myVar1);
+        System.out.println(myVar2);
+        System.out.println(myVar3);
+
+        System.out.println(Math.max(5, 10));
+
+        int xx = 10;
+        System.out.println(xx==10);
+
+        int myAge = 25;
+        int votingAge = 18;
+
+        if(myAge >= votingAge){
+            System.out.println("Old enough to vote!");
+        }else{
+            System.out.println("Not old enough to vote");
+        }
+
+
+        int time = 20;
+        String result = (time < 18) ? "Good day" : "Good evening";
+        System.out.println(result); */
+        
+/* 
+        // For Loop
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        for (String i : cars){
+            System.out.println(i);
+        }
+
+        // Break
+        for (int i=0;i<10;i++){
+            if(i==4){
+                break;
+            }
+            System.out.println(i);            
+        }
+
+        // Continue
+        for (int i = 0; i < 10; i ++){
+            if(i==4){
+                continue;
+            }
+            System.out.println(i);
+        }
+*/
+        // Array
+/* 
+        String[] cars2 = {"Volvo", "BMW", "Ford", "Mazda"};
+        System.out.println(cars2[0]);
+        cars2[0] = "Opel";
+        System.out.println(cars2[0]);
+        System.out.println(cars2.length); */
+
+
+        
+
+
+
     }
 }
