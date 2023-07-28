b2ffda0e84fc096b46fc539430fbbf8a75e7b7c1
Jake <lovelynike@naver.com>
2023-07-28 15:03:18 +0900

    20230728_1503

--- JAVA/java_W3S/W3S_Main.java
+++ JAVA/java_W3S/W3S_Main.java
@@ -105,5 +105,47 @@ public class W3S_Main {
         }
 
         //Java - What is OOP?
+/* 
+        OOP stands for Object-Oriented Programming.
+
+        Procedural programming is about writing procedures or methods that perform operations on the data, while object-oriented programming is about creating objects that contain both data and methods.
+
+        Object-oriented programming has several advantages over procedural programming:
+
+        OOP is faster and easier to execute
+        OOP provides a clear structure for the programs
+        OOP helps to keep the Java code DRY "Don't Repeat Yourself", and makes the code easier to maintain, modify and debug
+        OOP makes it possible to create full reusable applications with less code and shorter development time
+        Tip: The "Don't Repeat Yourself" (DRY) principle is about reducing the repetition of code. You should extract out the codes that are common for the application, and place them at a single place and reuse them instead of repeating it.
+ */
+        //Java - What are Classes and Objects?
+        //Classes and objects are the two main aspects of object-oriented programming.
+
+        
+        W3S_Main myObj = new W3S_Main();
+        System.out.println(myObj.x);
+      
+
+        myStaticMethod(); // Call the static method
+        // myPublicMethod(); This would complie an error
+
+        W3S_Main myObj2 = new W3S_Main(); // Create an object of Main
+        myObj2.myPublicMethod(); // Call the public method on the object
+
     }
+
+    int x = 15;
+
+    // the differences between static and public methods:
+    // Static method
+    static void myStaticMethod(){
+        System.out.println("Static methods can be called without creating objects");
+    }
+    // Public method
+    public void myPublicMethod(){
+        System.out.println("Public methods must be called by creating objects");
+    }
+
+
+
 }
---
+++ JAVA/java_W3S/W3S_Second.java
@@ -0,0 +1,7 @@
+class W3S_Second {
+  public static void main(String[] args) {
+    W3S_Main myObj = new W3S_Main();
+    myObj.x = 7;
+    System.out.println(myObj.x);
+  }
+}
