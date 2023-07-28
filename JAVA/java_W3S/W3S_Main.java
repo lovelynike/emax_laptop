7acf8ed1774d0dfa954dcac3248242160fb369e4
Jake <lovelynike@naver.com>
2023-07-28 16:55:40 +0900

    20230728_1655

--- JAVA/java_W3S/W3S_Main.java
+++ JAVA/java_W3S/W3S_Main.java
@@ -132,6 +132,17 @@ public class W3S_Main {
         W3S_Main myObj2 = new W3S_Main(); // Create an object of Main
         myObj2.myPublicMethod(); // Call the public method on the object
 
+        // Inside main, call the methods on the myCar object 
+        W3S_Main myCar = new W3S_Main();   // Create a myCar object
+        myCar.fullThrottle();      // Call the fullThrottle() method
+        myCar.speed(200);          // Call the speed() method
+    
+         W3S_Main myObj3 = new W3S_Main(); 
+        System.out.println(myObj3.y);
+
+        Person myPerson = new Person();
+        myPerson.setName("John"); // Set the value of the name variable to "John"
+        System.out.println(myPerson.getName());
     }
 
     int x = 15;
@@ -145,6 +156,24 @@ public class W3S_Main {
     public void myPublicMethod(){
         System.out.println("Public methods must be called by creating objects");
     }
+
+    // Create a fullThrottle() method
+    public void fullThrottle() {
+        System.out.println("The car is going as fast as it can!");
+    }
+
+    // Create a speed() method and add a parameter
+    public void speed(int maxSpeed) {
+        System.out.println("Max speed is: " + maxSpeed);
+    }
+
+     int y = 5;
+
+     // Create a class constructor for the Main class
+    public W3S_Main() {
+        y = 728;  // Set the initial value for the class attribute x
+    }
+
 
 
 
