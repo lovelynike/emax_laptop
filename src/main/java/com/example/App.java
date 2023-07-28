2752ba54ce55cb7306dacff1c0b7b39754f0d081
Jake <lovelynike@naver.com>
2023-07-28 09:38:24 +0900

    20230728

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -87,9 +87,16 @@ public class App
         System.out.println(cars2.length); */
 
 
-        
+        // Loop Through a Multi-Dimensional Array
+        int[][] myNumbers = {{1,2,3,4},{5,6,7}};
 
+        for (int i = 0; i < myNumbers.length; ++i){
+            for (int j = 0; j < myNumbers[i].length; ++j){
+                System.out.println(myNumbers[i][j]);
+            }
+        }
 
+        //Java - What is OOP?
 
     }
 }
